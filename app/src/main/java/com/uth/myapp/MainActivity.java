f605cece3bb6386e85d7bb1040e9960d61348bf8
etorrres <79731703+etorrres@users.noreply.github.com>
2024-01-24 20:46:56 -0600

    Actualizacion de App - 24/01/2024

--- app/src/main/java/com/uth/myapp/MainActivity.java
+++ app/src/main/java/com/uth/myapp/MainActivity.java
@@ -3,12 +3,32 @@ package com.uth.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nombre, apellidos, telefono;
+    Button btnproceso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        telefono = (EditText) findViewById(R.id.telefono);
+        btnproceso = (Button) findViewById(R.id.btnprocesar);
+
+        btnproceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), nombre.getText().toString(), Toast.LENGTH_LONG ).show();
+            }
+        });
+
     }
+
 }
